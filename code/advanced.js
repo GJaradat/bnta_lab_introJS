ee535d64ed8d2080be15a97e2647715a9ac627fe
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-03-11 15:27:01 +0000

    Completed advanced

--- code/advanced.js
+++ code/advanced.js
@@ -32,26 +32,42 @@ unitedKingdom = [
  * Q1. Add some tourist attractions to England
  */
 
-england = null;
+//This is just in case we don't know which index it is at:
+const englandIdx = unitedKingdom.findIndex(country => country.name === "England");
+unitedKingdom[englandIdx].touristAttractions = ["London Eye", "London Bridge"];
 
-// console.log(england);
-
+console.log("Added the London Eye and London Bridge to England attractions");
 /**
  * Q2. Change the capital of Wales to "Cardiff"
  */
 
-wales = null;
+const walesIdx = unitedKingdom.findIndex(country => country.name === "Wales");
+unitedKingdom[walesIdx].capital = "Cardiff";
 
-// console.log(wales);
+console.log("The capital of Wales has been changed to 'Cardiff'");
 
 /**
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
 
-northernIrelandKeys = null;
+const irelandIdx = unitedKingdom.findIndex(country => country.name === "Northern Ireland");
+irelandKeys = Object.keys(unitedKingdom[irelandIdx]);
 
-// console.log(northernIrelandKeys);
+console.log("The available keys for Ireland are: " + irelandKeys);
 
 /**
  * Q4. Use an if statement to compare the population of Scotland to the population of Wales, and say which is bigger.
  */
+
+function comparePopulation(country1,country2){
+  if(country1.population > country2.population){
+    return `${country1.name} has more people than ${country2.name}`;
+  } else {
+    return `${country2.name} has more people than ${country1.name}`;
+  }
+}
+
+const scotlandIdx = unitedKingdom.findIndex(country => country.name === "Scotland");
+console.log(comparePopulation(unitedKingdom[scotlandIdx],unitedKingdom[walesIdx]));
+
+// console.log(unitedKingdom);
